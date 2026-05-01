e682ce02758832a5ae972b43ae7a085952680b4f
Rayyan Salman <ryaansalman@gmail.com>
2026-05-01 14:36:42 +0500

    fix: increase nonce limit and adjust hash condition in findHash function

--- common.hpp
+++ common.hpp
@@ -49,9 +49,9 @@ string getMerkleRoot(const vector<string> &merkle) {
 pair<string,string> findHash(int index, string prevHash, vector<string> &merkle) {
     string header = to_string(index) + prevHash + getMerkleRoot(merkle);
     unsigned int nonce;
-    for (nonce = 0; nonce < 100000; nonce++ ) {
+    for (nonce = 0; nonce < 100000000000; nonce++ ) {
         string blockHash = sha256(header + to_string(nonce));
-        if (blockHash.substr(0,2) == "00"){
+        if (blockHash.substr(0,6) == "000000"){
             // cout << "nonce: " << nonce;
             // cout << "header: " << header;
             return make_pair(blockHash,to_string(nonce));
